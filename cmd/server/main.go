ff11847d622470471dc34b9545fb1e6f2c23e292
AbdKaan <abdu.kaan@gmail.com>
2025-12-27 16:06:46 +0300

    First commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const connectionString string = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf(
+			"Error occured while dialing with connection: %v with error: %v",
+			connectionString,
+			err,
+		)
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful!")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
