4a37d4c1465d7d095c0ec6f422b93007f68dcbf9
AbdKaan <abdu.kaan@gmail.com>
2026-01-03 15:37:36 +0300

    CH3 - L5: Transient Queues

---
+++ internal/pubsub/consume.go
@@ -0,0 +1,51 @@
+package pubsub
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange, queueName, key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := false
+	autoDelete := false
+	exclusive := false
+
+	switch queueType {
+	case SimpleQueueDurable:
+		durable = true
+	case SimpleQueueTransient:
+		autoDelete = true
+		exclusive = true
+	default:
+		log.Fatal("Queue needs to be durable or transient")
+	}
+
+	queue, err := channel.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+}
